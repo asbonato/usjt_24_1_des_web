99c00d721255d8addfb004a3382a9057e13f5d4a
asbonato <asbonato@gmail.com>
2024-03-26 20:51:22 -0300

    apostila(git-js): funcao multiplicacao implementada

--- calc.js
+++ calc.js
@@ -1,6 +1,9 @@
 module.exports = {
     soma: (x, y) => x + y,
-    subtracao: function(x ,y) {
+    subtracao: function(x, y) {
         return x - y
+    },
+    multiplicacao: (x, y) => {
+        return x * y
     },
 }
